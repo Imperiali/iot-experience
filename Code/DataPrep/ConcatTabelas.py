d6651002e32f485a302ccabd31efc89932f66053
Bruno Verdan Dantas Rodrigues <bruno.verdan@ed.company>
2021-12-13 20:44:43 -0300

    Adição dos datasets e script de concatenação

---
+++ Code/DataPrep/ConcatTabelas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+dataset_1_1_data = pd.read_csv("../../Data/Raw/01_Steps.csv")
+dataset_1_2_data = pd.read_csv("../../Data/Raw/02_Sleep.csv")
+
+dataset_1_data_certo = pd.merge(dataset_1_1_data, dataset_1_2_data, on='date', how='outer')
+
+dataset_1_data_certo.to_csv(r"../../Data/Processed/datasetConcatAT.csv", index=False)
+
